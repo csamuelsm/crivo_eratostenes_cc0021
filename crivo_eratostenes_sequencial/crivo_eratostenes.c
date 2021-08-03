3ac643d70af6a016b9addd657f19723314456bca
c1c3ro <cicerosousa205@gmail.com>
2021-08-03 15:27:58 -0300

    crivo serial compilado funcionando

--- crivo_eratostenes_sequencial/crivo_eratostenes.c
+++ crivo_eratostenes_sequencial/crivo_eratostenes.c
@@ -2,27 +2,26 @@
 #include <stdlib.h>
 
 
-int CrivoDeEratostenes(int n, int printaPrimos) {
+int CrivoDeEratostenes(int* vetor, int n, int printaPrimos) {
     // criando um vetor booleano para valores de 0 at� n
     // e setando todos os valores para true
     // este vetor ir� representar os valores que s�o primos (true) ou n�o (false)
-    int primos[n+1];
     for (int i = 0; i <= n; i++) {
-        primos[i] = 1;
+        vetor[i] = 1;
     }
 
     for (int p = 2; p*p <= n; p++) {
-        if (primos[p] == 1) {
+        if (vetor[p] == 1) {
             // "remover" op��es de primos
             for (int i = p*p; i <= n; i+=p) {
-                primos[i] = 0;
+                vetor[i] = 0;
             }
         }
     }
 
     int count_primos = 0;
     for (int i = 2; i <= n; i++) {
-        if (primos[i] == 1) {
+        if (vetor[i] == 1) {
             if (printaPrimos == 1){
                 printf("%d\t", i);
             }
@@ -35,7 +34,10 @@ int CrivoDeEratostenes(int n, int printaPrimos) {
 
 int main()
 {
-    int primos = CrivoDeEratostenes(10000000, 0);
-    printf("\nQuantidade de primos ate 10.000.000: %d\n", primos);
+    long int n;
+    scanf("%ld", &n);
+    int* primos = (int*) malloc(n * sizeof(int));
+    int n_primos = CrivoDeEratostenes(primos, n, 0);
+    printf("\nQuantidade de primos ate %ld: %d\n", n, n_primos);
     return 0;
 }
